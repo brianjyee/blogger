1acc4e927b3f2383e95e0eed3ca393e6ec0e2f82
Brian Yee <yee.brianj@gmail.com>
2014-05-31 22:56:08 -0400

    adding tagging

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,45 +1,45 @@
 class ArticlesController < ApplicationController
-	include ArticlesHelper
+    include ArticlesHelper
 
-	def index
-		@articles = Article.all
-	end
+    def index
+        @articles = Article.all
+    end
 
-	def show
-		@article = Article.find(params[:id])
+    def show
+        @article = Article.find(params[:id])
 
-		@comment = Comment.new
-		@comment.article_id = @article.id
-	end
+        @comment = Comment.new
+        @comment.article_id = @article.id
+    end
 
-	def new
-		@article = Article.new
-	end
+    def new
+        @article = Article.new
+    end
 
-	def create
-		@article = Article.new(article_params)
-		@article.save
+    def create
+        @article = Article.new(article_params)
+        @article.save
 
-		redirect_to article_path(@article)
-	end
+        redirect_to article_path(@article)
+    end
 
-	def destroy
-		@article = Article.find(params[:id])
-		@article.destroy
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
 
-		redirect_to articles_path
-	end
+        redirect_to articles_path
+    end
 
-	def edit
-		@article = Article.find(params[:id])
-	end
+    def edit
+        @article = Article.find(params[:id])
+    end
 
-	def update
-		@article = Article.find(params[:id])
-		@article.update(article_params)
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
 
-		flash.notice = "Article '#{@article.title}' Updated!"
+        flash.notice = "Article '#{@article.title}' Updated!"
 
-		redirect_to article_path(@article)
-	end
+        redirect_to article_path(@article)
+    end
 end
